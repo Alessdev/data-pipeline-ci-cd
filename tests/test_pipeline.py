aa3f7ff1bb1c6b2ef8c2e9f58bb160c2b1f71ab5
Dani Aless <alesroga.dev@gmail.com>
2026-01-21 22:42:58 -0500

    fix a letter missing in the code

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -3,9 +3,9 @@ from src.pipeline import transform_data
 
 def test_total_calculation():
     df = pd.DataFrame({
-         "price": [10,20],
-         "quantity": [2,3]
+         "price": [10, 20],
+         "quantity": [2, 3]
     })
     result = transform_data(df)
-    assert result["total].tolist() == [20,60]
+    assert result["total"].tolist() == [20, 60]
 
